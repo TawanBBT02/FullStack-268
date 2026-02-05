538c5d9dfa257013e1ad42b8a817b06050f210ef
TawanBBT02 <t0985595716@gmail.com>
2026-02-05 16:08:20 +0700

    sqlite3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 7000;
 
 app.get("/", (req, res) => {
-  res.send("Hello Warano Good World!");
+  res.send("Hello Waranon Good World!");
 });
 
 app.listen(port, () => {
